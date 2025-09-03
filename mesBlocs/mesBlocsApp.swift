4bed58489456c2f2f65135af3cf7772178bc4f1a
clementleclerc <leclerc92@gmail.com>
2025-09-03 14:29:33 +0200

    Initial Commit

---
+++ mesBlocs/mesBlocsApp.swift
@@ -0,0 +1,17 @@
+//
+//  mesBlocsApp.swift
+//  mesBlocs
+//
+//  Created by clement leclerc on 03/09/2025.
+//
+
+import SwiftUI
+
+@main
+struct mesBlocsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
