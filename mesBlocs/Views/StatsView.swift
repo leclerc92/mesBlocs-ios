4fe7fc140ab84c7f85e120b629707be688e9beba
clementleclerc <leclerc92@gmail.com>
2025-09-03 16:06:37 +0200

    ajout de empty dans toutes le vues

--- mesBlocs/Views/StatsView.swift
+++ mesBlocs/Views/StatsView.swift
@@ -6,10 +6,26 @@
 //
 
 import SwiftUI
+import _SwiftData_SwiftUI
 
 struct StatsView: View {
+    
+    @Environment(\.modelContext) private var modelContext
+    @Query(sort: \SessionModele.date, order: .reverse)
+    private var sessions: [SessionModele]
+    
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        NavigationView {
+            Group {
+                if sessions.isEmpty {
+                    EmptyStateView()
+                } else {
+                    
+                }
+            }
+            .navigationTitle("Création d'une session")
+            .navigationBarTitleDisplayMode(.large)
+        }
     }
 }
 
